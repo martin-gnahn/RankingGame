87ec4e81e3ea72470c188fe5c83da7db59a3099a
Martin Gnahn <martin@gnahn.de>
2026-06-12 11:30:46 +0200

    Initial commit

---
+++ src/test/java/com/example/miniamazon/MiniAmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniamazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAmazonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
