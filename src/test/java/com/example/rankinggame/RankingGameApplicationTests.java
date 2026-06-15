3dbdfb59e09a513a5c0b2abc88e6095112b60ba2
Martin Gnahn <martin@gnahn.de>
2026-06-15 13:36:08 +0200

    wip

--- src/test/java/com/example/rankinggame/RankingGameApplicationTests.java
+++ src/test/java/com/example/rankinggame/RankingGameApplicationTests.java
@@ -2,9 +2,24 @@ package com.example.rankinggame;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class RankingGameApplicationTests {
+    @Container
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    @DynamicPropertySource
+    static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void contextLoads() {
