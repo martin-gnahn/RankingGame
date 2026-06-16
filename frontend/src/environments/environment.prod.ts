08def1267b9483ca38f9c327a1cda81f2765131d
Martin Gnahn <martin@gnahn.de>
2026-06-16 11:42:26 +0200

    websocket PG-019

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   apiBaseUrl: '/api',
+  webSocketUrl: '/ws',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
   apiBaseUrl: 'http://localhost:8080/api',
+  webSocketUrl: 'ws://localhost:8080/ws',
 };
