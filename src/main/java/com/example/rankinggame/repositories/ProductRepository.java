4ecf271645a37444102806e67c6219f4e8bede2e
Martin Gnahn <martin@gnahn.de>
2026-06-17 10:34:06 +0200

    drop product

--- src/main/java/com/example/rankinggame/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.rankinggame.repositories;
-
-import com.example.rankinggame.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
